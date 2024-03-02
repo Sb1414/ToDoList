f05938a5a8cf0b2c8fe4d926a06fea0d91b06b52
Evette Isadora <kalkamanova.sabina14@gmail.com>
2024-03-03 00:43:57 +0300

    bug

--- ToDoList/ToDoList.DAL/AddDbContext.cs
+++ ToDoList/ToDoList.DAL/AddDbContext.cs
@@ -8,6 +8,7 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
+        Database.EnsureDeleted();
         Database.EnsureCreated();
     }
     
