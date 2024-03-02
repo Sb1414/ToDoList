f18a37effd20d45a67ef7e3632ab93d9f24f1027
Evette Isadora <kalkamanova.sabina14@gmail.com>
2024-03-03 00:02:59 +0300

    add db logic

---
+++ ToDoList/ToDoList.DAL/Intefaces/IBaseRepository.cs
@@ -0,0 +1,12 @@
+namespace ToDoList.DAL.Intefaces;
+
+public interface IBaseRepository<T>
+{
+    Task Create(T entity);
+
+    IQueryable<T> GetAll();
+    
+    Task Delete(T entity);
+    
+    Task<T> Update(T entity);
+}
